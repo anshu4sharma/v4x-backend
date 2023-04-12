d25c8feced446dfb4f536366abe8d53f8cd6d234
prashant-v-223 <prashantvadhvana@gmail.com>
2023-04-12 12:38:36 +0530

    ad

--- controllers/register.js
+++ controllers/register.js
@@ -111,7 +111,7 @@ exports.register = {
                       });
                     } else {
                       successResponse(res, {
-                        message: "registration completed successfully",
+                        message: "varification link has been send to your email address..!!",
                       });
                     }
                   });
@@ -169,7 +169,7 @@ exports.register = {
                       console.log(error);
                       if (!error) {
                         return successResponse(res, {
-                          message: "registration completed successfully",
+                          message: "varification link has been send to your email address..!!",
                         });
                       } else {
                         return badRequestResponse(res, {
