40d47631d27a6c0f4715e37d5d7ab05fe34338f7
prashant-v-223 <prashantvadhvana@gmail.com>
2023-04-14 13:43:59 +0530

    asd

--- controllers/admin.js
+++ controllers/admin.js
@@ -281,24 +281,13 @@ exports.admin = {
                 price: price,
               }
             );
-
-            let url = "https://geolocation-db.com/json/";
-
-            let options = {
-              method: "GET",
-              headers: {
-                Accept: "*/*",
-              },
-            };
-
-            fetch(url, options).then(async (res) => {
-              await V4XpriceSchemaDetails({
-                price: price,
-                ipAddress: res,
-              }).save();
-              return successResponse(res, {
-                message: "V4X price chenge successfully!",
-              });
+            const ipAddress = req.socket.remoteAddress;
+            await V4XpriceSchemaDetails({
+              price: price,
+              ipAddress: ipAddress,
+            }).save();
+            return successResponse(res, {
+              message: "V4X price chenge successfully!",
             });
           } else {
             return badRequestResponse(res, {
@@ -324,11 +313,12 @@ exports.admin = {
       if (decoded) {
         decoded = await cloneDeep(decoded);
         if (decoded.profile.username === "V4X10019") {
-          let data = await V4XpriceSchemaDetails.find({});
-          return successResponse(res, {
-            message: "V4X price chenge successfully!",
-            data: data,
-          });
+            let data = await V4XpriceSchemaDetails.find({});
+            return successResponse(res, {
+              message: "V4X price chenge successfully!",
+              data: data,
+            });
+         
         }
       } else {
         return badRequestResponse(res, {
